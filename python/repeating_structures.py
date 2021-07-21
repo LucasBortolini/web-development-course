bf4aa6f6bacceaa1bacba40653223c847bfebfa7
Lucas Sales <bortolinilucas20@gmail.com>
2021-07-21 18:01:17 -0300

    repeating structures and relational operators

---
+++ python/repeating_structures.py
@@ -0,0 +1,12 @@
+#x = 1
+
+#while x < 10:
+#    print (x)
+#    x = x+1
+
+listt = [1,2,3,4,5]
+listt2 = ["hello", "world", "!"]
+listt3 = [0, "hello", "9.99", "true"]
+
+for i in listt3:
+    print (i)
