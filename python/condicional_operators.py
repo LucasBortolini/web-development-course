fb89eccc8bbcc2bd2bb01219ec9ab66b2b41aea6
Lucas Sales <bortolinilucas20@gmail.com>
2021-07-21 17:53:16 -0300

    learning condicional operators

---
+++ python/condicional_operators.py
@@ -0,0 +1,29 @@
+#x = 1
+#y = 10000000000
+
+#if x > y:
+    #print ("x is bigger then y")
+
+#if y > x:
+    #print ("y is bigger then x")
+
+#x = 1
+#y = 2
+
+#if x > y:
+    #print ("x bigger then y")
+
+#else:
+    #print ("x is not bigger then y")
+
+x = 1 
+y = 2
+
+if x == y:
+    print ("equal numbers")
+
+elif y > x:
+    print ("y bigger then x")
+
+else:
+    print ("diferent numbers")
